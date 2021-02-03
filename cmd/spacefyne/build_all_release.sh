6ad109830bc8bd18941bebece70609582a5439db
Stuart Scott <stuart.murray.scott@gmail.com>
2021-02-03 10:39:11 -0800

    Bump version

--- cmd/spacefyne/build_all_release.sh
+++ cmd/spacefyne/build_all_release.sh
@@ -21,8 +21,8 @@ set -x
 go fmt $GOPATH/src/aletheiaware.com/spacefynego/...
 go vet $GOPATH/src/aletheiaware.com/spacefynego/...
 go test $GOPATH/src/aletheiaware.com/spacefynego/...
-fyne-cross android -app-id com.aletheiaware.space -app-version 1.1.4 -icon ./ui/data/logo.png -keystore=./private/SPACE.keystore -output SPACE -release ./cmd/spacefyne/
-fyne-cross darwin -app-id com.aletheiaware.space -app-version 1.1.4 -icon ./ui/data/logo.png -output SPACE -release ./cmd/spacefyne/
-fyne-cross ios -app-id com.aletheiaware.space -app-version 1.1.4 -icon ./ui/data/logo.png -output SPACE -release ./cmd/spacefyne/
-fyne-cross linux -app-id com.aletheiaware.space -app-version 1.1.4 -icon ./ui/data/logo.png -output space -release ./cmd/spacefyne/
-fyne-cross windows -app-id com.aletheiaware.space -app-version 1.1.4 -icon ./ui/data/logo.png -output space -release ./cmd/spacefyne/
+fyne-cross android -app-id com.aletheiaware.space -app-version 1.1.5 -icon ./ui/data/logo.png -keystore=./private/SPACE.keystore -output SPACE -release ./cmd/spacefyne/
+fyne-cross darwin -app-id com.aletheiaware.space -app-version 1.1.5 -icon ./ui/data/logo.png -output SPACE -release ./cmd/spacefyne/
+fyne-cross ios -app-id com.aletheiaware.space -app-version 1.1.5 -icon ./ui/data/logo.png -output SPACE -release ./cmd/spacefyne/
+fyne-cross linux -app-id com.aletheiaware.space -app-version 1.1.5 -icon ./ui/data/logo.png -output space -release ./cmd/spacefyne/
+fyne-cross windows -app-id com.aletheiaware.space -app-version 1.1.5 -icon ./ui/data/logo.png -output space -release ./cmd/spacefyne/
